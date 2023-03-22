a8155423b2a569349f9273355850502a6856edc4
mohamedibrahim135 <migif2003@gmail.com>
2023-03-21 19:07:06 -0700

    mo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+* add -Checks for lowercase character
+* @a: The character to be checked
+* @b: the some
+* Return: addtion of a and b
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
