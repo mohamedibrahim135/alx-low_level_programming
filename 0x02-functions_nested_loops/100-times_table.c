ab4175c2eced80d51a2af646e7b38c464d1973cb
mohamedibrahim135 <migif2003@gmail.com>
2023-03-22 14:24:23 -0700

    mohamed

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include"main,h"
+#include<stdio.h>
+/**
+* print_times_table -Checks for lowercase character
+* @n: The character to be checked
+* Return: 1 for lowercase character or 0 for anything else
+*/
+void print_times_table(int n)
+{
+int mul, i, j;
+
+if (n > 0 && n < 15)
+{
+for (j = 0; j <= n; j++)
+{
+for (i = 0; i <= n; i++)
+{
+
+mul = j * i;
+
+if (i < n)
+{
+printf("%i, ", mul);
+}
+
+else
+{
+printf("%i", mul);
+}
+}
+printf("\n");
+}
+}
+}
