76f8dab556db498297a0099a5a0863f14c38de05
mohamedibrahim135 <migif2003@gmail.com>
2023-03-24 07:46:27 -0700

    mihaemd

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
 /**
 *main - Prints a Fizz Buzz program
 *Return: Always 0 (Success)
 */
-
 int main(void)
 {
 int x;
-
 for (x = 1; x <= 100; x++)
 {
-
 if (x % 3 == 0)
 {
 if (x % 5 == 0)
