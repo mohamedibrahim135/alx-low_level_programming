d0962cfd692ede326c243d239ac7b6145565f708
mohamedibrahim135 <migif2003@gmail.com>
2023-03-27 17:44:07 -0700

    f

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,19 +9,19 @@ int _atoi(char *s)
 int c = 0;
 unsigned int ni = 0;
 int min = 1;
-int isi = 0:
+int isi = 0;
 
 while (s[c])
 {
-if (s [c] == 45)
+if (s[c] == 45)
 {
 min *= -1;
-{
-while (s [c] >= 48 && s [c] <= 57)
 }
+while (s[c] >= 48 && s[c] <= 57)
+{
 isi = 1;
-ni = (ni * 10) + (s [c] - '0');
-C++;
+ni = (ni * 10) + (s[c] - '0');
+c++;
 }
 if (isi == 1)
 {
