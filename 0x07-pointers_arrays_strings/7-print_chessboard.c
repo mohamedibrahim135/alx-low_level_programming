909835888a6812a6749610b970911ec09443fcb3
mohamedibrahim135 <migif2003@gmail.com>
2023-04-03 18:37:14 -0700

    d

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
-*_print_chessboard - Entry point
-* da: array
+*print_chessboard - Entry point
+* @a: array
 * Return: Always 0 (Success)
 */
 void print_chessboard(char (*a)[8])
