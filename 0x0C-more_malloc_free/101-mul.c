7ff0420b1833839dc6a9896b7b79206db3b521a2
mohamedibrahim135 <migif2003@gmai;.com>
2023-04-14 19:38:21 -0700

    d

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -43,7 +43,7 @@ exit(98);
 }
 /**
 * main - multiplies two positive numbers
-* @arge: number of arguments
+* @argc: number of arguments
 * @argv: array of arguments
 *
 * Return: always 0 (Success)
@@ -62,31 +62,31 @@ result = malloc(sizeof(int) * len);
 if (!result)
 return (1);
 for (i = 0; i <= len1 + len2; i++)
-result [i] = 0;
+result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
 {
-digit1 = s1 [len1] - '0';
+digit1 = s1[len1] - '0';
 carry = 0;
 for (len2 = _strlen (s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 *digit2);
-result [len1 + len2 + 1] = carry % 10;
+carry += result[len1 + len2 + 1] + (digit1 * digit2);
+result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
 if (carry > 0)
-result [len1 + len2 + 1] += carry;
+result[len1 + len2 + 1] += carry;
 }
 for (i = 0; i < len - 1; i++)
 {
-if (result [i])
+if (result[i])
 a = 1;
 if (a)
-_putchar(result [i] + '0');
+_putchar(result[i] + '0');
 }
 if (!a)
 _putchar('0');
-_putchar ('\n');
+_putchar('\n');
 free(result);
 return (0);
 }
