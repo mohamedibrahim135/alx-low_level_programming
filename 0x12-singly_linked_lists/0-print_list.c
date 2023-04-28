bf4fe032004ed5241fa55dc29c7a578fcdd49d65
mohamedibrahim135 <migif2003@gmail.com>
2023-04-28 16:25:00 -0700

    d

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,13 +4,13 @@
 * @s: the string whose length to check
 * Return: integer length of string
 */
-int _strlen (char *s)
+int _strlen(char *s)
 {
 int i = 0;
 if (!s)
 return (0);
 while (*s++)
-i++
+i++;
 return (i);
 }
 /**
@@ -18,14 +18,14 @@ return (i);
 * @h: pointer to first node
 * Return: size of list
 */
-size_t print_list (const list_t *h)
+size_t print_list(const list_t *h)
 {
 size_t i = 0;
 while (h)
 {
 printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 h = h->next;
-i++:
+i++;
 }
-return (i);  
+return (i);
 }
