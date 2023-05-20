de400bbce104944fd5dd91618b581f9f883dbbd9
mohamedibrahim135 <migif2003@gmail.com>
2023-05-19 19:43:20 -0700

    d

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,9 +23,9 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t bytes = 0, len = _strlen(text_content);
-if (filename)
+if (!filename)
 return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 if (len)
